1e2dca82a0734691bbf5648851407b1a48c271e9
PratikChakrabortyaz <117933667+PratikChakrabortyaz@users.noreply.github.com>
2023-09-23 12:45:21 +0530

    Update Basic queue operations.c

--- Queues/Implementation using array/Basic queue operations.c
+++ Queues/Implementation using array/Basic queue operations.c
@@ -1,75 +1,71 @@
-/*Basic queue operations. Not menu driven*/
-#include<stdio.h>
-#include<stdbool.h>
+/*Basic queue queue operations. Not menu driven*/
+#include <stdio.h>
+#include <stdbool.h>
 #define MAX 3
 
-typedef struct
-{
+typedef struct {
     int rear;
     int front;
     int queue[MAX];
-}Queue;
+} Queue;
 
-void initialize(Queue *q)
-{
-    q->rear=-1;
-    q->front=-1;
+void initialize(Queue *q) {
+    q->rear = -1;
+    q->front = -1;
 }
-bool isEmpty(Queue *q)
-{
-    return q->rear==q->front;
+
+bool isEmpty(Queue *q) {
+    return q->front == -1;
 }
-bool isFull(Queue *q)
-{
-    return q->rear==MAX-1;
+
+bool isFull(Queue *q) {
+    return q->rear == MAX - 1;
 }
-void enqueue(Queue *q,int ele)
-{
-    if(isFull(q))
-    {
+
+void enqueue(Queue *q, int ele) {
+    if (isFull(q)) {
         printf("\nQueue full\n");
-    }
-    else{
-        q->queue[++(q->rear)]=ele;
+    } else {
+        if (isEmpty(q)) {
+            q->front = 0;
+        }
+        q->queue[++(q->rear)] = ele;
     }
 }
-int dequeue(Queue *q)
-{
-    if(isEmpty(q))
-    {
+
+int dequeue(Queue *q) {
+    if (isEmpty(q)) {
         printf("\nQueue empty\n");
-    }
-    else
-    {
-        return q->queue[++(q->front)];
+    } else {
+        int dequeuedElement = q->queue[(q->front)++];
+        if (q->front > q->rear) {
+            initialize(q);
+        }
+        return dequeuedElement;
     }
 }
-void display(Queue *q)
-{
-    if(isEmpty(q))
-    {
+
+void display(Queue *q) {
+    if (isEmpty(q)) {
         printf("\nQueue contents empty\n");
-    }
-    else
-    {
-        for(int i=q->front;i<=q->rear;i++)
-        {
-            printf("%d\n",q->queue[i]);
+    } else {
+        for (int i = q->front; i <= q->rear; i++) {
+            printf("%d\n", q->queue[i]);
         }
     }
 }
 
-void main()
-{
+int main() {
     Queue q;
     initialize(&q);
     dequeue(&q);
-    enqueue(&q,3);
-    enqueue(&q,7);
-    enqueue(&q,8);
-    enqueue(&q,2);
+    enqueue(&q, 3);
+    enqueue(&q, 7);
+    enqueue(&q, 8);
+    enqueue(&q, 2);
     dequeue(&q);
     display(&q);
 
-
+    return 0;
 }
+
